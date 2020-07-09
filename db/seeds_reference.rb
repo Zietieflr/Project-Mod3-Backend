21600b2274fecfe2c480897adea05ce8f0dfd05b
Zietieflr <lrmcguire93@gmail.com>
2020-07-09 09:41:38 -0600

    Seed category database. Make and test show and index get requests.

--- db/seeds_reference.rb
+++ db/seeds_reference.rb
@@ -1,12 +1,3 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
-
-Category.destroy_all
 
 categories = 'aircraft carrier
 airplane
@@ -355,7 +346,3 @@ zebra
 zigzag'.split("\n")
 
 capital_categories = categories.map(&:capitalize)
-
-capital_categories.each do |title|
-  Category.create(title: title)
-end
